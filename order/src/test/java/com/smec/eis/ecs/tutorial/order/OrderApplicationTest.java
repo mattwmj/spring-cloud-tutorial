8f0bea1569d7f99863361dde211885ef1936b868
WANGMJ <wangminjie@smec-cn.com>
2019-04-09 15:23:09 +0800

    initial commit with order and warehouse service pom

---
+++ order/src/test/java/com/smec/eis/ecs/tutorial/order/OrderApplicationTest.java
@@ -0,0 +1,18 @@
+package com.smec.eis.ecs.tutorial.order;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
